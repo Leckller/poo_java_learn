2403f16e20d71d0c98b6c14b5b535f046fc8972b
Leckller <7leckler@gmail.com>
2024-02-28 21:20:50 -0300

    feat: class pessoa and main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Main {
+  public static void main(String[] args) {
+    Pessoa ruyPessoa = new Pessoa("ruyzinho", 19);
+    ruyPessoa.getPessoa();
+    ruyPessoa.getTrabalho();
+  }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,34 @@
+package src;
+
+public class Pessoa {
+  public Pessoa(String nome, int idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  public Pessoa(String nome, int idade, String trabalho, int salario) {
+    this.nome = nome;
+    this.idade = idade;
+    this.trabalho = trabalho;
+    this.salario = salario;
+  };
+
+  protected int idade;
+  protected String nome;
+  private String trabalho;
+  private int salario = 0;
+
+  public void getTrabalho() {
+    if (trabalho == null || salario == 0) {
+      System.out.println("Esta pessoa está desempregada");
+    } else {
+      System.out.println("Trabalho: " + this.trabalho);
+      System.out.println("Salario: " + this.salario);
+    }
+  }
+
+  public void getPessoa() {
+    System.out.println("Nome: " + this.nome);
+    System.out.println("Idade: " + this.idade);
+  }
+}
